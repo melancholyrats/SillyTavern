b561239fdab5ccb42c4ec0f14007b53da63c4efc
maver <kentucky@posteo.de>
2023-06-25 21:21:32 +0200

    Allow the user to toggle names in chat history

--- public/script.js
+++ public/script.js
@@ -4112,7 +4112,6 @@ function changeMainAPI() {
         getHordeModels();
     }
 
-    console.log(oai_settings.chat_completion_source)
     switch (oai_settings.chat_completion_source) {
         case chat_completion_sources.OPENAI:
             console.log('Setting up OpenAI prompt manager');
